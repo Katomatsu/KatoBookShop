3dfbefd009aa9f2cd409306e11d7e38690b930fc
Katomatsu <adogairbekov@gmail.com>
2024-03-05 18:40:05 +0400

    integrated ejd template engine

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('./util/path')
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(rootDir, 'public')))
 
-app.use('/admin', adminRouter)
+app.use('/admin', adminData.routes)
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''})
 })
 
 app.listen(8888);
